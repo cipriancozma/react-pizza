2a1d56764b53d5fed1c905cfee56f0b047595d9c
Ciprian <cozma_ciprian19@yahoo.com>
2023-06-13 00:10:19 +0300

    refactored code

--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -4,15 +4,23 @@ import Pizza from "./Pizza";
 
 function PizzaList() {
   return (
-    <div className="pizzas">
-      {pizzaData.length > 0 ? (
-        pizzaData.map((pizza, idx) => {
-          return <Pizza key={idx} pizza={pizza} />;
-        })
-      ) : (
-        <p>Sorry! We're working on our menu. Please come back later!</p>
-      )}
-    </div>
+    <>
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All
+        organic, all delicious.
+      </p>
+      <div className="pizzas">
+        {pizzaData.length > 0 ? (
+          <>
+            {pizzaData.map((pizza, idx) => {
+              return <Pizza key={idx} pizza={pizza} />;
+            })}
+          </>
+        ) : (
+          <p>Sorry! We're working on our menu. Please come back later!</p>
+        )}
+      </div>
+    </>
   );
 }
 
